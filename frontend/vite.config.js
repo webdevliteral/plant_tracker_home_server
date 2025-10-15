33ac1c4fae1cb6597cdb9c7e9da2c938e082a573
Devin Wood <pro.devinwood@gmail.com>
2025-10-14 23:40:08 -0400

    Updated vite config for https

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    https: {
+      key: fs.readFileSync('./key.pem'),
+      cert: fs.readFileSync('./cert.pem'),
+    },
     host: '0.0.0.0',
-    port: 5173,
+    port: 5173
   },
   build: {
     outDir: 'dist',
